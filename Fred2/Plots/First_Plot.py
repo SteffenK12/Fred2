0be4c96dc44f45b838dfe20ccaa88c704896bcbb
Steffen Kienzler <steffen.kienzler@student.uni-tuebingen.de>
2015-11-06 11:05:32 +0100

    Test

--- Fred2/Plots/First_Plot.py
+++ Fred2/Plots/First_Plot.py
@@ -1,20 +1,36 @@
+from copy import deepcopy
+
 __author__ = 'Steffen'
 import matplotlib.pyplot as plt
+import math
+import numpy as np
 
-days = [1,2,3,4,5]
+def f(x,y):
+    return x
 
-sleeping = [7,8,6,11,7]
-eating = [2,3,4,3,2]
-working = [7,8,7,2,2]
-playing = [8,5,7,8,13]
+x = np.array([1, 2, 3, 4, 5, 6, 7])
+y = np.array([2, 4, 7, 7, 7, 4, 2])
+#X, Y = np.meshgrid(x,y)
+#Z = f(x,y)
 
-slices = [7,2,2,13]
-activities = ['sleeping','eating','working','playing']
-cols= ['c','m','r','b']
-plt.pie(slices, labels=activities,colors=cols,startangle=90, shadow=True, explode=(0,0.1,0,0),autopct='%1.1f%%')
+
+
+
+low = min(y)
+high = max(y)
+
+colormap = plt.get_cmap('Reds')
+fig = plt.figure()
+ax1 = plt.subplot2grid((1, 1), (0, 0))
+
+#plt.pcolormesh(X, Y, Z, cmap=colormap)
+ax1.plot(x, y, "-", label='random data')
+ax1.grid(True)
 #plt.xlabel('x')
 #plt.ylabel('y')
-plt.title('Interesting Graph')
+plt.ylim([0, math.ceil(high + 0.5 * (high - low))])
+plt.subplots_adjust(top=0.95)
+plt.title('Test Graph')
 #plt.legend()
 plt.show()
 
